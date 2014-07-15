ee3f569b75744d9b06323503662e08587700b24b
bhagyashree <bwaval@tavisca.com>
2014-07-15 18:28:43 +0530

    commit after modifications according to review1

--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -10,47 +10,49 @@ namespace BowlingFixtures
         public void TestMethod1()
         {
             Bowling.Game obj = new Bowling.Game();
-            int[] arr1 = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
-            for (int i = 0; i < arr1.Length; i++)
-                obj.Roll(arr1[i]);
-            Assert.AreEqual(obj.GetScore(), 300);
+            for (int i = 0; i < 3; i++)
+                obj.Roll(10);
+            Assert.AreEqual(obj.GetScore(), 60);
         }
         [TestMethod]
         public void TestMethod2()
         {
             Bowling.Game obj = new Bowling.Game();
-            int[] arr2 = new int[] { 10, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5 };
-            for (int i = 0; i < arr2.Length; i++)
-                obj.Roll(arr2[i]);
+            obj.Roll(10);
+            for (int i = 0; i < 9; i++) { obj.Roll(3); obj.Roll(5); }
             Assert.AreEqual(obj.GetScore(), 98);
         }
         [TestMethod]
         public void TestMethod3()
         {
             Bowling.Game obj = new Bowling.Game();
-            int[] arr3 = new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
-            for (int i = 0; i < arr3.Length; i++)
-                obj.Roll(arr3[i]);
-            Assert.AreEqual(obj.GetScore(), 200);
+            for (int i = 0; i < 20; i++)
+                obj.Roll(5);
+            Assert.AreEqual(obj.GetScore(), 190);
         }
         [TestMethod]
         public void TestMethod4()
         {
             //check score is correct till fourth frame
             Bowling.Game obj = new Bowling.Game();
-            int[] arr4 = new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
             for (int i = 0; i < 8; i++)
-                obj.Roll(arr4[i]);
+                obj.Roll(5);
             Assert.AreEqual(obj.GetScore(), 70);
         }
         public void TestMethod5()
         {
-            //check score is correct till fourth frame
             Bowling.Game obj = new Bowling.Game();
-            int[] arr2 = new int[] { 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 10, 3, 5, 3, 5 };
-            for (int i = 0; i < arr2.Length; i++)
-                obj.Roll(arr2[i]);
+            for (int i = 0; i < 9; i++) { obj.Roll(3); obj.Roll(5); }
+            obj.Roll(10);
+            for (int i = 0; i < 2; i++) { obj.Roll(3); obj.Roll(5); }
             Assert.AreEqual(obj.GetScore(), 98);
+        }
+        public void TestMethod6()
+        {
+            Bowling.Game obj = new Bowling.Game();
+            for (int i = 0; i < 12; i++)
+                obj.Roll(10);
+            Assert.AreEqual(obj.GetScore(), 300);
         }
     }
 }
