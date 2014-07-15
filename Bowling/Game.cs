ba008d73916de9ce254070835835cb5ebeda35b8
bhagyashree <bwaval@tavisca.com>
2014-07-15 14:48:05 +0530

    final commit for bowling game code with test cases

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,14 +8,66 @@ namespace Bowling
 {
     public class Game
     {
+        int frame_cnt = 0;
+        int[] frame_score = new int[12];
+        int[] frame_status = new int[12];
+        int prev = -1;
+        int score = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            manage_score(pins);
+            //throw new NotImplementedException();
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int final_score = score;
+            //Console.Write("hi");
+            for (int i = 0; i < frame_cnt; i++)
+                Console.Write("{0}\t", frame_score[i]);
+            for (int i = 0; i < frame_cnt; i++)
+            {
+                if (i < 9)
+                {
+                    if (frame_status[i] == 1)
+                        final_score = final_score + frame_score[i + 1];
+                    else if (frame_status[i] == 2)
+                        final_score = final_score + frame_score[i + 1] + frame_score[i + 2];
+                }
+            }
+
+            return final_score;
+            //throw new NotImplementedException();
+        }
+
+        public void manage_score(int pins)
+        {
+
+            score = score + pins;
+            if (prev == -1)
+            {
+                if (pins == 10)
+                {
+                    frame_status[frame_cnt] = 2;
+                    frame_score[frame_cnt] = pins;
+                    frame_cnt = frame_cnt + 1;
+                }
+                else
+                {
+                    prev = pins;
+                }
+            }
+            else
+            {
+                frame_score[frame_cnt] = prev + pins;
+                prev = -1;
+                if (frame_score[frame_cnt] == 10)
+                    frame_status[frame_cnt] = 1;
+                else
+                    frame_status[frame_cnt] = -1;
+                frame_cnt += 1;
+            }
+
         }
 
     }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -9,6 +9,48 @@ namespace BowlingFixtures
         [TestMethod]
         public void TestMethod1()
         {
+            Bowling.Game obj = new Bowling.Game();
+            int[] arr1 = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            for (int i = 0; i < arr1.Length; i++)
+                obj.Roll(arr1[i]);
+            Assert.AreEqual(obj.GetScore(), 300);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Bowling.Game obj = new Bowling.Game();
+            int[] arr2 = new int[] { 10, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5 };
+            for (int i = 0; i < arr2.Length; i++)
+                obj.Roll(arr2[i]);
+            Assert.AreEqual(obj.GetScore(), 98);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Bowling.Game obj = new Bowling.Game();
+            int[] arr3 = new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+            for (int i = 0; i < arr3.Length; i++)
+                obj.Roll(arr3[i]);
+            Assert.AreEqual(obj.GetScore(), 200);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //check score is correct till fourth frame
+            Bowling.Game obj = new Bowling.Game();
+            int[] arr4 = new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+            for (int i = 0; i < 8; i++)
+                obj.Roll(arr4[i]);
+            Assert.AreEqual(obj.GetScore(), 70);
+        }
+        public void TestMethod5()
+        {
+            //check score is correct till fourth frame
+            Bowling.Game obj = new Bowling.Game();
+            int[] arr2 = new int[] { 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 10, 3, 5, 3, 5 };
+            for (int i = 0; i < arr2.Length; i++)
+                obj.Roll(arr2[i]);
+            Assert.AreEqual(obj.GetScore(), 98);
         }
     }
 }
